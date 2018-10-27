2a32d376a5babcb9a6cdc5f383458ba9ead9214b
Michael Plazek <michael.plazek91@gmail.com>
2018-10-27 12:28:22 -0600

    Add bid routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,9 @@
 const askRoutes = require('./ask_routes');
 const userRoutes = require('./user_routes');
+const bidRoutes = require('./bid_routes');
 
 module.exports = function(app, db) {
     askRoutes(app, db);
     userRoutes(app, db);
+    bidRoutes(app, db);
 };

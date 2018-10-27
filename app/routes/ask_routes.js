84cfc136bbeacecdcfbcf89a857f5f955690960f
Michael Plazek <michael.plazek91@gmail.com>
2018-10-27 12:34:14 -0600

    Ran prettier on all files

--- app/routes/ask_routes.js
+++ app/routes/ask_routes.js
@@ -1,84 +1,83 @@
-const ObjectID = require('mongodb').ObjectID;
+const ObjectID = require("mongodb").ObjectID;
 
 module.exports = function(app, db) {
-
-	// GET all asks
-    app.get('/asks', (req, res) => {
-        db.collection('asks').find({}, (err, data) => {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                data.toArray((err, datum) => {
-                    res.send(datum);
-                })
-            }
+  // GET all asks
+  app.get("/asks", (req, res) => {
+    db.collection("asks").find({}, (err, data) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        data.toArray((err, datum) => {
+          res.send(datum);
         });
+      }
     });
+  });
 
-		// GET an ask where query param id = _id
-    app.get('/ask', (req, res) => {
-        const details = { '_id':  new ObjectID(req.query.id) };
-            db.collection('asks').findOne(details, (err, item) => {
-                if (err) {
-                res.send({'error':'An error has occurred'});
-                } else {
-                    res.send(item);
-                }
-            });
-        });
-
-		// POST a new ask
-    app.post('/asks', (req, res) => {
-        const post = {
-					coin: req.body.coin,
-					owner: req.body.owner,
-					price: req.body.price,
-					volume: req.body.volume,
-					lat: req.body.lat,
-					lng: req.body.lng,
-					timestamp: new Date(),
-				};
-        db.collection('asks').insert(post, (err, result) => {
-            if (err) {
-                res.send({ 'error': 'An error has occurred' });
-            } else {
-                res.send(result.ops[0]);
-            }
-        });
+  // GET an ask where query param id = _id
+  app.get("/ask", (req, res) => {
+    const details = { _id: new ObjectID(req.query.id) };
+    db.collection("asks").findOne(details, (err, item) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send(item);
+      }
     });
+  });
 
-		// DELETE an ask where query param id = _id
-    app.delete('/ask', (req, res) => {
-        const id = req.query.id;
-        const details = { '_id': new ObjectID(id) };
-        db.collection('asks').remove(details, (err, item) => {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                res.send('Post ' + id + ' deleted!');
-            }
-        });
+  // POST a new ask
+  app.post("/asks", (req, res) => {
+    const post = {
+      coin: req.body.coin,
+      owner: req.body.owner,
+      price: req.body.price,
+      volume: req.body.volume,
+      lat: req.body.lat,
+      lng: req.body.lng,
+      timestamp: new Date()
+    };
+    db.collection("asks").insert(post, (err, result) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send(result.ops[0]);
+      }
     });
+  });
 
-		// PUT update an existing ask where query param id = _id
-    app.put('/ask', (req, res) => {
-        const id = req.query.id;
-        const details = { '_id': new ObjectID(id) };
-        const post = {
-					coin: req.body.coin,
-					owner: req.body.owner,
-					price: req.body.price,
-					volume: req.body.volume,
-					lat: req.body.lat,
-					lng: req.body.lng,
-					timestamp: new Date(),
-				};
-        db.collection('asks').update(details, post, (err, result) => {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                res.send(post);
-            }
-        });
+  // DELETE an ask where query param id = _id
+  app.delete("/ask", (req, res) => {
+    const id = req.query.id;
+    const details = { _id: new ObjectID(id) };
+    db.collection("asks").remove(details, (err, item) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send("Post " + id + " deleted!");
+      }
     });
+  });
+
+  // PUT update an existing ask where query param id = _id
+  app.put("/ask", (req, res) => {
+    const id = req.query.id;
+    const details = { _id: new ObjectID(id) };
+    const post = {
+      coin: req.body.coin,
+      owner: req.body.owner,
+      price: req.body.price,
+      volume: req.body.volume,
+      lat: req.body.lat,
+      lng: req.body.lng,
+      timestamp: new Date()
+    };
+    db.collection("asks").update(details, post, (err, result) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send(post);
+      }
+    });
+  });
 };
--- app/routes/bid_routes.js
+++ app/routes/bid_routes.js
@@ -1,84 +1,81 @@
-
-
 module.exports = function(app, db) {
+  // GET all the bids
+  app.get("/bids", (req, res) => {
+    db.collection("bids").find({}, (err, data) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        data.toArray((err, datum) => {
+          res.send(datum);
+        });
+      }
+    });
+  });
 
-	// GET all the bids
-	app.get('/bids', (req, res) => {
-		db.collection('bids').find({}, (err, data) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				data.toArray((err, datum) => {
-					res.send(datum);
-				})
-			}
-		});
-	});
+  // GET a bid where query param id = _id
+  app.get("/bid", (req, res) => {
+    const details = { _id: new ObjectID(req.query.id) };
+    db.collection("bids").findOne(details, (err, item) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send(item);
+      }
+    });
+  });
 
-	// GET a bid where query param id = _id
-	app.get('/bid', (req, res) => {
-		const details = { '_id':  new ObjectID(req.query.id) };
-		db.collection('bids').findOne(details, (err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(item);
-			}
-		});
-	});
+  // POST a new bid
+  app.post("/bids", (req, res) => {
+    const post = {
+      coin: req.body.coin,
+      owner: req.body.owner,
+      price: req.body.price,
+      volume: req.body.volume,
+      lat: req.body.lat,
+      lng: req.body.lng,
+      timestamp: new Date()
+    };
+    db.collection("bids").insert(post, (err, result) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
 
-	// POST a new bid
-	app.post('/bids', (req, res) => {
-		const post = {
-			coin: req.body.coin,
-			owner: req.body.owner,
-			price: req.body.price,
-			volume: req.body.volume,
-			lat: req.body.lat,
-			lng: req.body.lng,
-			timestamp: new Date(),
-		};
-		db.collection('bids').insert(post, (err, result) => {
-			if (err) {
-				res.send({ 'error': 'An error has occurred' });
-			} else {
-				res.send(result.ops[0]);
-			}
-		});
-	});
+  // DELETE a bid where query param id = _id
+  app.delete("/bid", (req, res) => {
+    const id = req.query.id;
+    const details = { _id: new ObjectID(id) };
+    db.collection("bids").remove(details, (err, item) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send("Bid " + id + " deleted.");
+      }
+    });
+  });
 
-	// DELETE a bid where query param id = _id
-	app.delete('/bid', (req, res) => {
-		const id = req.query.id;
-		const details = { '_id': new ObjectID(id) };
-		db.collection('bids').remove(details, (err, item) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send('Bid ' + id + ' deleted.');
-			}
-		});
-	});
-
-	// PUT update an existing bid where query param id = _id
-	app.put('/bid', (req, res) => {
-		const id = req.query.id;
-		const details = { '_id': new ObjectID(id) };
-		const post = {
-			coin: req.body.coin,
-			owner: req.body.owner,
-			price: req.body.price,
-			volume: req.body.volume,
-			lat: req.body.lat,
-			lng: req.body.lng,
-			timestamp: new Date(),
-		};
-		db.collection('bids').update(details, post, (err, result) => {
-			if (err) {
-				res.send({'error':'An error has occurred'});
-			} else {
-				res.send(post);
-			}
-		});
-	});
+  // PUT update an existing bid where query param id = _id
+  app.put("/bid", (req, res) => {
+    const id = req.query.id;
+    const details = { _id: new ObjectID(id) };
+    const post = {
+      coin: req.body.coin,
+      owner: req.body.owner,
+      price: req.body.price,
+      volume: req.body.volume,
+      lat: req.body.lat,
+      lng: req.body.lng,
+      timestamp: new Date()
+    };
+    db.collection("bids").update(details, post, (err, result) => {
+      if (err) {
+        res.send({ error: "An error has occurred" });
+      } else {
+        res.send(post);
+      }
+    });
+  });
 };

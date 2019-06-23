4f99ab27d65c6a45c098165fe8af61707f3e7e4c
Michael Plazek <michael.plazek91@gmail.com>
2019-06-22 21:03:27 -0600

    Add route for tour visited

--- app/routes/config_routes.js
+++ app/routes/config_routes.js
@@ -3,9 +3,27 @@ module.exports = function(app, db) {
 
   const Configuration = db.collection("configuration");
 
+  // POST when the user has selected Dark Mode
   app.post("/config/mode", (req, res) => {
     const details = { userId: req.body.userId };
     const updates = {$set: { dark: req.body.dark }};
+    const options = { upsert: true, returnOriginal: false };
+    console.log(req.body);
+    Configuration.findOneAndUpdate(details, updates, options, (err, item) => {
+      if (err) {
+        res.send({ error: "Error retrieving configuration" });
+      } else {
+        if (item) {
+          return res.send(item.value)
+        }
+      }
+    });
+  });
+
+  // POST when the user has completed the tour
+  app.post("/config/visited", (req, res) => {
+    const details = { userId: req.body.userId };
+    const updates = {$set: { visited: req.body.visited }};
     const options = { upsert: true, returnOriginal: false };
     console.log(req.body);
     Configuration.findOneAndUpdate(details, updates, options, (err, item) => {
